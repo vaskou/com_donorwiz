0b5dde2f89ef0277b77d8220114f54a3162a3aec
vaskou <vaskou@hotmail.com>
2015-03-20 15:27:48 +0200

    Update 20/03/2015 15:27

---
+++ components/com_donorwiz/controllers/ajax.php
@@ -0,0 +1,27 @@
+<?php
+defined('_JEXEC') or die;
+require_once JPATH_COMPONENT . '/controller.php';
+class DonorwizControllerAjax extends DonorwizController {
+    public function getLayout() {
+	
+		//Check access token
+		JSession::checkToken( ) or die( 'Invalid Token' );
+		
+		$jinput = JFactory::getApplication()->input;
+		
+		$layout = $jinput->get('layout', '', 'string');
+		$layoutPath = $jinput->get('layoutPath', '', 'base64');
+		$layoutParams = json_decode(  htmlspecialchars_decode ( $jinput->get('layoutParams', '', 'html') ) );
+		try
+		{
+			echo new JResponseJson( JLayoutHelper::render( $layout , (array) $layoutParams , base64_decode ( $layoutPath ) , null ) );
+		}
+		catch(Exception $e)
+		{
+			echo new JResponseJson($e);
+		}
+	
+		jexit();
+    }
+	
+}
--- components/com_donorwiz/layouts/popup/popup-button.php
+++ components/com_donorwiz/layouts/popup/popup-button.php
@@ -1,23 +1,81 @@
 <?php
-
 // no direct access
 defined('_JEXEC') or die;
-
 $buttonText = ( isset ( $displayData['buttonText'] ) ) ? $displayData['buttonText'] : '' ;
 $buttonIcon = ( isset ( $displayData['buttonIcon'] ) ) ? $displayData['buttonIcon'] : '' ;
 $buttonType = ( isset ( $displayData['buttonType'] ) ) ? $displayData['buttonType'] : 'uk-button-primary' ;
 $buttonID = ( isset ( $displayData['buttonID'] ) ) ? $displayData['buttonID'] : uniqid() ;
-
 $popupHeader = ( isset ( $displayData['popupParams']['header'] ) ) ? $displayData['popupParams']['header'] : null ;
 $popupFooter = ( isset ( $displayData['popupParams']['footer'] ) ) ? $displayData['popupParams']['footer'] : '' ;
-
 $layoutPath = $displayData['layoutPath'];
 $layoutName = $displayData['layoutName'];
 $layoutParams = $displayData['layoutParams'];
+$buttonLink = ( isset ( $displayData['buttonLink'] ) ) ? $displayData['buttonLink'] : '#' ;
+$isAjax = ( isset ( $displayData['isAjax'] ) ) ? true : null ;
+$styles=( isset ( $displayData['styles'] ) ) ? $displayData['styles']  : array() ;
+$scripts=( isset ( $displayData['scripts'] ) ) ? $displayData['scripts'] : array() ;
 
+if( $isAjax )
+{
+foreach($styles as $style){
+	JHtml::stylesheet($style);
+}
+foreach($scripts as $script){
+	JHtml::script($script);
+}
+
+JHtml::_('jquery.framework');
+JHtml::_('behavior.formvalidator');
+$script = array();
+$script[] = 'jQuery(function($) {';
+	
+$script[] = '	var $ = jQuery.noConflict();';
+	
+$script[] = '	$(document).ready(function() {';
+$script[] = '		$( "#modal-'.$buttonID.'.uk-modal" ).on({';
+$script[] = '			"show.uk.modal": function(){';
+$script[] = '				var layoutWrapperIsEmpty=( $( "#modal-'.$buttonID.'.uk-modal .modal-content .layout-wrapper" ).children().length=="0" ) ? true : false ;';
+$script[] = '				if( layoutWrapperIsEmpty == true ){';
+$script[] = '					var postdata={';
+$script[] = '						"'.JSession::getFormToken().'":"1",';
+$script[] = '						"layout":"'.$layoutName.'",';
+$script[] = '						"layoutPath":"'.base64_encode ( $layoutPath ).'",';
+$script[] = '						"layoutParams":"'.htmlspecialchars ( json_encode( $layoutParams ) ) .'",';
+$script[] = '						"return":"'.base64_encode ( JFactory::getURI()->toString() ) .'"';
+$script[] = '					};';
+$script[] = '					$.ajax({';
+$script[] = '						type: "POST",';
+$script[] = '						url: "index.php?option=com_donorwiz&task=ajax.getLayout",';
+$script[] = '						data: postdata';
+$script[] = '					}).done( function(response) {';
+$script[] = '						try{';
+$script[] = '							var response = jQuery.parseJSON( response );';
+$script[] = '							$( "#modal-'.$buttonID.'.uk-modal .modal-content .spinner-wrapper" ).toggleClass("uk-hidden");';
+$script[] = '							$( "#modal-'.$buttonID.'.uk-modal .modal-content .layout-wrapper" ).html(response.data);';
+$script[] = '							document.formvalidator = new JFormValidator();';
+$script[] = '							$( "#modal-'.$buttonID.'.uk-modal .modal-content .layout-wrapper" ).toggleClass("uk-hidden");';
+$script[] = '						}';
+$script[] = '						catch(err) {';
+$script[] = '							$( "#modal-'.$buttonID.'.uk-modal" ).hide();console.log(response);';
+$script[] = '							$.UIkit.notify( "<i class=uk-icon-warning></i> '.JText::_('COM_DONORWIZ_POPUP_ERROR').'"+err , { timeout:20000 } );';
+$script[] = '					}';
+$script[] = '					})';
+$script[] = '					.fail(function() {';
+$script[] = '						$( "#modal-'.$buttonID.'.uk-modal" ).hide();';
+$script[] = '						$.UIkit.notify( "<i class=uk-icon-warning></i> '.JText::_('COM_DONORWIZ_POPUP_ERROR').'" , { timeout:2000 } );';
+$script[] = '					});';
+$script[] = '				}';
+$script[] = '			},';
+$script[] = '			"hide.uk.modal": function(){';
+$script[] = '			 }';
+$script[] = '		});';
+$script[] = '	});';
+$script[] = '});';
+JFactory::getDocument()->addScriptDeclaration(implode("\n", $script));
+}
 ?>
 
-<a href="#modal-<?php echo $buttonID;?>" class="<?php echo $buttonType;?>" data-uk-modal ><i class="<?php echo $buttonIcon;?>"></i><?php echo $buttonText;?></a>
+<a href="<?php echo $buttonLink;?>" class="<?php echo $buttonType;?>" data-uk-modal="{target:'#modal-<?php echo $buttonID;?>'}" ><i class="<?php echo $buttonIcon;?>"></i><?php echo $buttonText;?></a>
 
 <div id="modal-<?php echo $buttonID;?>" class="uk-modal" style="display:none;">
 
@@ -30,9 +88,21 @@ $layoutParams = $displayData['layoutParams'];
 			<?php echo $popupHeader;?>
 		</div>		
 		<?php endif; ?>
-
-		<?php echo JLayoutHelper::render($layoutName, $layoutParams , $layoutPath , null ); ?>
-
+		
+		<div class="modal-content" data-uk-observe>
+			
+			<?php if( $isAjax ):?>
+				<div class="uk-text-center uk-margin-large-top spinner-wrapper">
+					<i class="uk-icon-spinner uk-icon-spin uk-icon-large"></i>
+					<h3><?php echo JText::_('COM_DONORWIZ_MODAL_PLEASE_WAIT');?></h3>
+				</div>
+				<div class="layout-wrapper uk-hidden"></div>
+			<?php else:?>
+				<?php echo JLayoutHelper::render($layoutName, $layoutParams , $layoutPath , null ); ?>
+			<?php endif;?>
+			
+		</div>
+	
 	</div>
 
 </div>
