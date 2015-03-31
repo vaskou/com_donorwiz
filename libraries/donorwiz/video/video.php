8f17e142aa6e0f59f05961283795b7c04f71f5bf
vaskou <vaskou@hotmail.com>
2015-03-31 16:30:11 +0300

    Update 31/03/2015 16:30

--- libraries/donorwiz/video/video.php
+++ libraries/donorwiz/video/video.php
@@ -54,4 +54,30 @@ class DonorwizVideo {
 	
 	}
 	
+	public function getYouTubeVideoHTMLForArticles($url)
+	{
+		if($url){
+			$html = array();
+			
+			preg_match('/[\\?\\&]v=([^\\?\\&]+)/', $url, $matches);
+			
+			if( isset ($matches[1]) ){
+				$id = $matches[1];
+				$width = '800px';
+				$height = '450px';
+				
+				$html[] = '<div style="position:relative;padding-bottom:56.25%;padding-top:25px;height:0;">';
+				$html[] = '<iframe id="ytplayer" type="text/html" width="'.$width.'" height="'.$height.'" style="position:absolute;top:0;left:0;width:100%;height:100%;" ';
+				$html[] = 'src="https://www.youtube.com/embed/'.$id.'?rel=0&showinfo=0&color=white&iv_load_policy=3" ';
+				$html[] = 'frameborder="0" allowfullscreen></iframe> ';
+				$html[] = '</div>';
+				
+				return implode($html);
+			}
+		}
+		
+		return false;
+	
+	}
+	
 }
