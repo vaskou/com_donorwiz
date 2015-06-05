32c87b7d142afe6314fef29ea20518a6f4d73e97
yesinternet <dev.yesinternet@gmail.com>
2015-06-05 14:23:31 +0300

    Language file
    - Added lang strings
    - Minor change to social email footer

--- components/com_donorwiz/layouts/mail/common/social.php
+++ components/com_donorwiz/layouts/mail/common/social.php
@@ -4,7 +4,6 @@
 
 <div style="padding:10px 0 10px 0;background-color:#e5e5e5;">
 <div style="max-width:320px;margin:0 auto;">
-<h3 style="text-align:center;"><?php echo JText::_('COM_DONORWIZ_EMAIL_CONNECT');?></h3>
 <h3 style="text-align:center;margin:5px 0;padding:5px 0;background: #3b5998;"><a target="_blank" href="https://www.facebook.com/DONORwiz" style="color:#ffffff;text-decoration:none;display:block !important">facebook</a></h3>
 <h3 style="text-align:center;margin:5px 0;padding:5px 0;background: #00acee;"><a target="_blank" href="https://twitter.com/DONORwiz" style="color:#ffffff;text-decoration:none;display:block !important">twitter</a></h3>
 <h3 style="text-align:center;margin:5px 0;padding:5px 0;background: #DB4A39;"><a target="_blank" href="https://plus.google.com/u/0/116814216754971857234/about" style="color:#ffffff;text-decoration:none;display:block !important">google+</a></h3>
